01f424baa6c2e6e8eb00b11d1524f2bac4d7af96
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2018-06-23 17:00:03 +0900

    [add] componensts/moleculesにMenuListを追加した

---
+++ src/client/components/molecules/MenuList/index.jsx
@@ -0,0 +1,41 @@
+// @flow
+import * as React from "react";
+import styles from "./style.css";
+import MenuButton from "../../atoms/MenuButton";
+
+type PropType = {
+  current: string,
+  onButtonClick: (current: string) => void
+};
+
+const items: Array<string> = ["入退室処理", "ユーザ登録", "利用者一覧"];
+
+function MenuList(props: PropType): React.Node {
+  const { current, onButtonClick } = props;
+
+  const listItems: Array<React.Node> = items.map((item: string): React.Node => {
+    const menuButton: React.Node =
+      item === current ? (
+        <MenuButton
+          selected="selected"
+          onButtonClick={() => {
+            onButtonClick(item);
+          }}
+        >
+          {item}
+        </MenuButton>
+      ) : (
+        <MenuButton
+          onButtonClick={() => {
+            onButtonClick(item);
+          }}
+        >
+          {item}
+        </MenuButton>
+      );
+    return <li key={item}>{menuButton}</li>;
+  });
+
+  return <ul className={styles.menuList}>{listItems}</ul>;
+}
+export default MenuList;
--- src/client/components/storybook/index.jsx
+++ src/client/components/storybook/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import MenuButton from "../atoms/MenuButton";
 
+// actions
+import MenuButton from "../atoms/MenuButton";
+// molecules
+import MenuList from "../molecules/MenuList";
+
+// #region atoms
 storiesOf("Atoms/MenuButton", module)
   .add("通常", () => (
     <MenuButton onButtonClick={action("clicked")}>メニュー</MenuButton>
@@ -12,3 +17,17 @@ storiesOf("Atoms/MenuButton", module)
       メニュー
     </MenuButton>
   ));
+// #endregion atoms
+
+// #region molecules
+storiesOf("Components/MenuList", module)
+  .add("入退室処理 選択時", () => (
+    <MenuList current="入退室処理" onButtonClick={action("clicked")} />
+  ))
+  .add("ユーザ登録 選択時", () => (
+    <MenuList current="ユーザ登録" onButtonClick={action("clicked")} />
+  ))
+  .add("利用者一覧 選択時", () => (
+    <MenuList current="利用者一覧" onButtonClick={action("clicked")} />
+  ));
+// #endregion molecules
